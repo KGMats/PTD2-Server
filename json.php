e28e4b8f5f349ad741a76936baa9783bcbddc5e1
KGMats <kaykygibran2@gmail.com>
2021-11-15 23:37:18 -0300

    Renamed Utils.php to json.php
    Renamed Utils.php to json.php

---
+++ json.php
@@ -0,0 +1,55 @@
+<?php
+function account_exists($email): bool
+{
+    $accounts = get_accounts();
+    foreach ($accounts as $account)
+    {
+        if ($account['email'] == $email)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+function get_account($email, $pass)
+{
+    $accounts = get_accounts();
+    foreach ($accounts as $account)
+    {
+        if ($account['email'] === $email && $account['pass'] === $pass)
+        {
+            return $account;
+        }
+    }
+    return null;
+}
+
+function get_accounts(): array
+{
+    $accounts_file = '../../accounts.json';
+    $accounts = array();
+    if (file_exists($accounts_file))
+    {
+        $content = file_get_contents($accounts_file);
+        $accounts = json_decode($content, true);
+    }
+    return $accounts;
+}
+
+function update_account_data($email, $pass, $new_data): bool
+{
+    $accounts = get_accounts();
+    foreach ($accounts as $index => $account)
+    {
+        if ($account['email'] === $email && $account['pass'] === $pass)
+        {
+            $accounts[$index] = array_merge($accounts[$index], $new_data);
+            $data = json_encode($accounts);
+            file_put_contents('../../accounts.json', $data);
+            return true;
+        }
+    }
+    return false;
+}
+?>
--- public/php/ptd2_save_12.php
+++ public/php/ptd2_save_12.php
@@ -1,34 +1,32 @@
 <?php
 
-require_once '../../Utils.php';
+require_once '../../json.php';
 
-function create_account($email, $pass)
+function create_account($email, $pass): string
 {
     if (account_exists($email))
     {
-        return "Result=Failure&Reason=taken";
+        return 'Result=Failure&Reason=taken';
     }
     $accounts = get_accounts();
     $account = ['email' => $email, 'pass' => $pass];
     array_push($accounts, $account);
     $data = json_encode($accounts);
-    $handle = fopen("../../accounts.json", "w");
-    fwrite($handle, $data);
-    fclose($handle);
+    file_put_contents('../../accounts.json', $data);
     return load_account($email, $pass);
 }
 
-function load_account($email, $pass)
+function load_account($email, $pass): string
 {
     if (get_account($email, $pass))
     {
         return 'Result=Success&Reason=loadedAccount';
     }
-    return "Result=Failure&Reason=NotFound";
+    return 'Result=Failure&Reason=NotFound';
 }
 
 
-function load_story($email, $pass)
+function load_story($email, $pass): string
 {
     # For now this function is not complete, so the game does not recognize
     # the response as a valid response, softlocking the game on the
@@ -42,7 +40,7 @@ function load_story($email, $pass)
             $new_account = false;
             $profile = $account["profile{$i}"];
             echo "PN{$i}={$profile['Nickname']}&";
-            echo "extra=ycm&";
+            echo 'extra=ycm&';
 
             echo 'dw=' . Date('w') . '&';
 
@@ -56,14 +54,15 @@ function load_story($email, $pass)
     }
     if ($new_account)
     {
-        echo "Result=Success&extra=ycm";
+        return 'Result=Success&extra=ycm';
     }
 }
 
 
-function save_story($email, $pass)
+function save_story($email, $pass): string
 {
     $new_data = array();
+    $whichProfile = $_POST['whichProfile'];
     parse_str($_POST['extra'], $save_info);
 
 
@@ -74,21 +73,21 @@ function save_story($email, $pass)
 
     if(isset($save_info['MapSave']))
     {
-        $new_data["profile{$_POST['whichProfile']}"]['MapLoc'] = $save_info['MapLoc'];
-        $new_data["profile{$_POST['whichProfile']}"]['MapSpot'] = $save_info['MapSpot'];
+        $new_data["profile{$whichProfile}"]['MapLoc'] = $save_info['MapLoc'];
+        $new_data["profile{$whichProfile}"]['MapSpot'] = $save_info['MapSpot'];
     }
 
     if(isset($save_info['MSave']))
     {
-        $new_data["profile{$_POST['whichProfile']}"]['Money'] = $save_info['MA'];
+        $new_data["profile{$whichProfile}"]['Money'] = $save_info['MA'];
     }
 
     if(isset($save_info['TimeSave']))
     {
-        $new_data["profile{$_POST['whichProfile']}"]['CurrentTime'] = $save_info['CT'];
+        $new_data["profile{$whichProfile}"]['CurrentTime'] = $save_info['CT'];
     }
 
-    if(isset($save_info['saveD']))
+    if(isset($_POST['needD']))
     {
         for ($gen = 1; $gen <= 6; $gen++)
         {
@@ -96,13 +95,17 @@ function save_story($email, $pass)
         }
     }
 
+    $new_data["profile"]['poke'] = decode_pokeinfo($_POST['extra3']);
+    $new_data['inventory'] = decode_inventory($_POST['extra4']);
+    
+
     update_account_data($email, $pass, $new_data);
-    return "Result=Success";
+    return 'Result=Success';
 }
 
 function load_1v1($email, $pass)
 {
-    echo "Result=Success&extra=wyyyycycmye&extra2=yqym";
+    return 'Result=Success&extra=wyyyycycmye&extra2=yqym';
 }
 
 
